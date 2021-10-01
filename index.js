0cb4cebce22dd76981ec1a44581d96a39203b98a
Ashish <ashish.goyal@nexxbase.com>
2021-10-01 14:16:16 +0530

    dotenv added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv').config();
 const cors = require('cors')
 const csv = require('csvtojson');
 const bodyParser = require('body-parser');
